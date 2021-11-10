053138636906f320dbf5a25bc065a9ef983f7ba6
Maxim Movchan <maksmovchan@icloud.com>
2021-11-10 09:53:09 +0300

    '#'

--- lib/data/resources/images.dart
+++ lib/data/resources/images.dart
@@ -13,3 +13,16 @@ abstract class AppImages{
 
   static const String yoga = 'assets/icons/yoga.png';
 }
+
+abstract class HabitsImages{
+  static const String brush = 'assets/icons/brush&floss.png';
+  static const String drinkWater = 'assets/icons/drinkwater.png';
+  static const String eatFruits = 'assets/icons/eatfruits.png';
+  static const String excersize = 'assets/icons/excersize.png';
+  static const String fallAsleepEarly = 'assets/icons/fallasleepearly.png';
+  static const String lessCaffeine = 'assets/icons/lesscaffeine.png';
+  static const String read = 'assets/icons/read.png';
+  static const String stretch = 'assets/icons/stretch.png';
+  static const String takeAShower = 'assets/icons/takeashower.png';
+  static const String wakeEarly = 'assets/icons/wakeearly.png';
+}
